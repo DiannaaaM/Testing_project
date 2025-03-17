9c0bac5c1233e031f000d9556cd19da4cbd3d708
DiannaaaMmrrr <murlaevadiana@gmail.com>
2025-03-17 23:42:38 +0300

    init project and add HW files

---
+++ lesson_1_task_ 3.py
@@ -0,0 +1,4 @@
+first_name = input()
+last_name = input()
+
+print(f"Вас зовут: {first_name} {last_name}")
---
+++ lesson_1_task_5.py
@@ -0,0 +1,5 @@
+def return_nums(num: int) -> int:
+    return num
+
+for i in range( 0, 11):
+    print(return_nums(88005553535))
