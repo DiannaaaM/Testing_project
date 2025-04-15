d0ac8202e3941a9869961ae6c56129bcb39d951a
DiannaaaMm <murlaevadiana@gmail.com>
2025-04-15 14:24:53 +0300

    fix some mistakes

--- lesson5/lesson05_task3.py
+++ lesson5/lesson05_task3.py
@@ -1,18 +1,5 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-
-driver.get('http://the-internet.herokuapp.com/inputs')
-
-input_field = driver.find_elements_by_tag_name('input')[0]
-input_field.send_keys('Sky')
-
-input_field.clear()
-
-input_field.send_keys('Pro')
-
-driver.quit()
-
 driver = webdriver.Firefox()
 
 driver.get('http://the-internet.herokuapp.com/inputs')
