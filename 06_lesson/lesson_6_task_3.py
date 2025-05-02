bd6762cd0186b08d4da201054358930171f4337b
DiannaaaMm <murlaevadiana@gmail.com>
2025-05-02 13:16:59 +0300

    fix

--- 06_lesson/lesson_6_task_3.py
+++ 06_lesson/lesson_6_task_3.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,15 +8,21 @@ driver = webdriver.Chrome()
 try:
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-    images = WebDriverWait(driver, 20).until(
+    images = WebDriverWait(driver, 40).until(
         EC.presence_of_all_elements_located((By.TAG_NAME, "img"))
     )
-    time.sleep(15)
 
-    if len(images) >= 3:
-        src_value = images[2].get_attribute("src")
-        print(src_value)
-    else:
-        print("Меньше 3 изображений на странице.")
+    third_image = images[2]
+
+    WebDriverWait(driver, 40).until(
+        lambda d: third_image.get_attribute("src") and third_image.get_attribute("src") != ""
+    )
+
+    src_value = third_image.get_attribute("src")
+
+    print(src_value)
+
+except Exception as e:
+    print("Произошла ошибка:", e)
 finally:
     driver.quit()
