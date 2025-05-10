1147c5fd4a320ae348932aeb2a79511f8a6f5c31
DiannaaaMm <murlaevadiana@gmail.com>
2025-05-10 11:41:42 +0300

    add negative tests

--- 08_lesson/yougile_GET_project.py
+++ 08_lesson/yougile_GET_project.py
@@ -2,13 +2,27 @@ import pytest
 import requests
 
 
-@pytest.mark.positive
+@pytest.mark.support
 def test_GET_response():
     id = 'c6011640-857f-4209-a2ab-af21d95fd30c'
     url = f'https://ru.yougile.com/api-v2/projects/{id}'
     token = 'YOUR_TOKEN'
     headers = {
         "Authorization": f"Bearer {token}",
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+
+
+@pytest.mark.xfail(reason='Намеренный провал')
+def test_GET_response_fail():
+    id = 'c6011640-857f-4209-a2ab-af21d95fd30c'
+    url = f'https://ru.yougile.com/api-v2/projects/{id}'
+    token = 'YOUR_TOKEN'
+    headers = {
+        "Authorization": f"{token}",
         'Content-Type': 'application/json'
     }
 
--- 08_lesson/yougile_PUT_project.py
+++ 08_lesson/yougile_PUT_project.py
@@ -1,10 +1,31 @@
 import requests
 import pytest
-from pycparser.ply.yacc import token
 
 
-@pytest.mark.positive
+@pytest.mark.support
 def test_PUT_request():
+    id = 'c6011640-857f-4209-a2ab-af21d95fd30c'
+    url = f'https://ru.yougile.com/api-v2/projects/{id}'
+    token = 'YOUR_TOKEN'
+    headers = {
+        "Authorization": f"Bearer {token}",
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "deleted": True,
+        "title": "Del project",
+        "users": {
+            "1c95b193-78b2-4095-99af-323c6c730bc0": "worker"
+        }
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+    assert response.status_code == 200
+
+
+
+@pytest.mark.xfail(reason='Намеренный провал')
+def test_PUT_request_fail():
     id = 'c6011640-857f-4209-a2ab-af21d95fd30c'
     url = f'https://ru.yougile.com/api-v2/projects/{id}'
     token = 'YOUR_TOKEN'
